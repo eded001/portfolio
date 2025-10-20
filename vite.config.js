f9a51351c839cffab68ec2cae5cd184aaa0afcdc
Ed <108343328+eded001@users.noreply.github.com>
2025-10-20 10:02:16 -0300

    chore: desinstalei vite-svgr

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,12 @@ import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite';
-import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    svgr()
   ],
   resolve: {
     alias: {
